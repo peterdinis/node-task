f9826b23e56cc85c0a01ffcd6530bda46551527c
Peter Dinis <peterdinis0@pobox.sk>
2024-08-13 13:23:09 +0200

    FOO

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -2,10 +2,20 @@ import { Request, Response } from 'express';
 import { Product } from '../interfaces/productInterface';
 import { fetchProducts } from '../services/productsService';
 
-
 // controller for getting all products
-
 export const getProducts = async (req: Request, res: Response) => {
+    // Validate query parameters
+    const allowedParams = ['minPrice', 'maxPrice'];
+    const queryParams = Object.keys(req.query);
+    
+    const invalidParams = queryParams.filter(param => !allowedParams.includes(param));
+
+    if (invalidParams.length > 0) {
+        return res.status(400).json({
+            error: `Invalid query parameters: ${invalidParams.join(', ')}. Only ${allowedParams.join(', ')} are allowed.`
+        });
+    }
+
     const minPrice = parseFloat(req.query.minPrice as string) || 0;
     const maxPrice = parseFloat(req.query.maxPrice as string) || 100000;
 
